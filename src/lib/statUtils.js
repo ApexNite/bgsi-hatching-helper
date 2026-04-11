1ec8728c21f614fe34e2f97321dd3e7c6609a2e5
ApexNite <128202315+ApexNite@users.noreply.github.com>
2026-04-11 11:41:50 -0500

    Make xl 50% easier

--- src/lib/statUtils.js
+++ src/lib/statUtils.js
@@ -3,13 +3,20 @@ import { dataStore, isDataLoaded } from "./dataStore.js";
 import { D, toNumber, add, mul, div, max } from "./mathDecimal.js";
 
 const DEFAULT_XL_CHANCE_BY_RARITY = Object.freeze({
-  infinity: 1 / 500,
-  secret: 1 / 500,
-  legendary: 1 / 2000,
-  epic: 1 / 10000,
-  rare: 1 / 25000,
-  unique: 1 / 75000,
-  common: 1 / 100000,
+  // infinity: 1 / 500,
+  // secret: 1 / 500,
+  // legendary: 1 / 2000,
+  // epic: 1 / 10000,
+  // rare: 1 / 25000,
+  // unique: 1 / 75000,
+  // common: 1 / 100000,
+  infinity: (1 / 500) * 1.5,
+  secret: (1 / 500) * 1.5,
+  legendary: (1 / 2000) * 1.5,
+  epic: (1 / 10000) * 1.5,
+  rare: (1 / 25000) * 1.5,
+  unique: (1 / 75000) * 1.5,
+  common: (1 / 100000) * 1.5,
 });
 
 export function calculateStats(sources, toggles, numbers) {
