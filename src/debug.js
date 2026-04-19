1b212cbfcda2cd6acbd2ddd23b98e59b687a31b5
ApexNite <128202315+ApexNite@users.noreply.github.com>
2026-04-18 23:35:45 -0500

    Add debug flag system

--- src/debug.js
+++ src/debug.js
@@ -1,6 +1,9 @@
 // @ts-nocheck
 import { get } from "svelte/store";
 import { dataStore, isDataLoaded, dataError } from "./lib/dataStore.js";
+import { setCookie, getCookie } from "./lib/cookieUtils.js";
+
+let flags = {};
 
 export function getProjectHash() {
   console.log(__PROJECT_HASH__);
@@ -23,6 +26,23 @@ export function getBuildDate() {
   console.log(__BUILD_DATE__);
 }
 
+export function setFlag(flag) {
+  flags = getCookie("debug-flags") || {};
+  flags[flag] = true;
+  setCookie("debug-flags", flags);
+}
+
+export function unsetFlag(flag) {
+  flags = getCookie("debug-flags") || {};
+  flags[flag] = false;
+  setCookie("debug-flags", flags);
+}
+
+export function getFlag(flag) {
+  flags = getCookie("debug-flags") || {};
+  return Object.prototype.hasOwnProperty.call(flags, flag) ? flags[flag] : null;
+}
+
 function canReadData() {
   return !get(dataError) && get(isDataLoaded);
 }
@@ -33,5 +53,8 @@ if (typeof window !== "undefined") {
     getSourceHash,
     getDataHash,
     getProjectHash,
+    setFlag,
+    unsetFlag,
+    getFlag,
   };
 }
