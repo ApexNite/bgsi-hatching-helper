59f05e66399c657b13e6912fe32dfc9821ea158b
ApexNite <128202315+ApexNite@users.noreply.github.com>
2026-06-26 12:08:43 -0700

    Add 2x super legendary and xl chance

--- src/lib/statUtils.js
+++ src/lib/statUtils.js
@@ -15,13 +15,13 @@ import {
 } from "./mathDecimal.js";
 
 const DEFAULT_XL_CHANCE_BY_RARITY = Object.freeze({
-  infinity: (1 / 500) * 1.5,
-  secret: (1 / 500) * 1.5,
-  legendary: (1 / 2000) * 1.5,
-  epic: (1 / 10000) * 1.5,
-  rare: (1 / 25000) * 1.5,
-  unique: (1 / 75000) * 1.5,
-  common: (1 / 100000) * 1.5,
+  infinity: (1 / 500) * 2,
+  secret: (1 / 500) * 2,
+  legendary: (1 / 2000) * 2,
+  epic: (1 / 10000) * 2,
+  rare: (1 / 25000) * 2,
+  unique: (1 / 75000) * 2,
+  common: (1 / 100000) * 2,
 });
 
 export function calculateStats(sources, toggles, numbers) {
@@ -59,7 +59,7 @@ export function calculateStats(sources, toggles, numbers) {
     baseInfinityLuck: 0,
     baseShinyChance: 1 / 40,
     baseMythicChance: 1 / 100,
-    baseSuperLegendaryChance: 1 / 2500,
+    baseSuperLegendaryChance: (1 / 2500) * 2,
     baseHatchSpeed: 0,
     _applyAdjustedShiny: hasGoldenEggMastery(sources),
     _burstBlessingLevel: getBurstBlessingLevel(sources),
@@ -169,7 +169,7 @@ export function calculateManualStats(manualStats, sources, numbers) {
     baseInfinityLuck: manualStats.infinityLuck || 1,
     baseShinyChance: 1 / manualStats.shinyChance,
     baseMythicChance: 1 / manualStats.mythicChance,
-    baseSuperLegendaryChance: 1 / 2500,
+    baseSuperLegendaryChance: (1 / 2500) * 2,
     baseHatchSpeed: manualStats.hatchSpeed / 100,
   };
 
